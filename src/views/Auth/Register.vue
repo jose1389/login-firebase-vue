8e70cc01c8847d2c81350cd0b42e1bf65a2cf265
jose1389 <joseantonio1389.jag@gmail.com>
2021-07-29 11:21:36 -0600

    formulario

--- src/views/Auth/Register.vue
+++ src/views/Auth/Register.vue
@@ -1,14 +1,34 @@
 <template>
-  <div>Register</div>
+  <div class="container">
+    <div class="columns">
+      <div class="colum is-6">
+        <form action="#">
+          <div class="field">
+            <label class="label">Name</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="e.g Alex Smith" />
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Email</label>
+            <div class="control">
+              <input
+                class="input"
+                type="email"
+                placeholder="e.g. alexsmith@gmail.com"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'Register',
-  components: {
-
-  }
-}
+  name: "Register",
+  components: {},
+};
 </script>
