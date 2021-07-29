29a6efc3ed247770215f196a5f2ef51fe7622516
jose1389 <joseantonio1389.jag@gmail.com>
2021-07-29 11:33:06 -0600

    button-form

--- src/views/Auth/Register.vue
+++ src/views/Auth/Register.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="container">
     <div class="columns">
-      <div class="colum is-6">
-        <form action="#">
+      <div class="colum is-6 is-offset-3">
+        <h3 class="title is-3">Crear Cuenta</h3><hr>
+        <form action="#" @submit.prevent="register">
           <div class="field">
             <label class="label">Name</label>
             <div class="control">
-              <input class="input" type="text" placeholder="e.g Alex Smith" />
+              <input class="input" type="text" placeholder="Samuel Lopez" />
             </div>
           </div>
 
@@ -16,10 +17,24 @@
               <input
                 class="input"
                 type="email"
-                placeholder="e.g. alexsmith@gmail.com"
+                placeholder="ejemplo@gmail.com"
               />
             </div>
           </div>
+
+          <div class="field">
+            <label class="label">Password</label>
+            <div class="control">
+              <input
+                class="input"
+                type="password"
+                placeholder="password"
+              />
+            </div>
+          </div>
+
+          <button type="submit" class="button is-primary">Registrarme</button>
+
         </form>
       </div>
     </div>
@@ -29,6 +44,11 @@
 <script>
 export default {
   name: "Register",
+  methods:{
+    register(){
+      console.log('clic')
+    }
+  },
   components: {},
 };
 </script>
